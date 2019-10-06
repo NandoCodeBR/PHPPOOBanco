8be53f7dafa0527f91dcffee7974685ed6c8352e
NandoCodeBR <52359881+NandoCodeBR@users.noreply.github.com>
2019-10-06 14:07:46 -0300

    Update ContaBanco.php

--- ContaBanco.php
+++ ContaBanco.php
@@ -11,13 +11,16 @@
  *
  * @author Família Muniz
  */
+
+// Criação da classe ContaBanco
+
 class ContaBanco {
    public $numConta;
    protected $tipo;
    private $dono;
    private $saldo;
    private $status;
-   
+// Getters e Setters para cada atributo   
    function GetnumConta(){
       return $this->numConta;
    }
@@ -48,6 +51,10 @@ class ContaBanco {
     function setStatus($stat){
         $this->status=$stat;
     }
+   
+/* Função abrirConta, sempre que criar um objeto ContaBanco, essa função deverá ser chamada
+para definir se vai ser uma conta corrente ou uma conta poupança e definir o seu status para ativo  
+*/
     function abrirConta($t){
         $this->setTipo($t);
         $this->setStatus(true);
@@ -60,6 +67,9 @@ class ContaBanco {
             echo "Conta criada com sucesso!";
         }
     }
+/* Função fecharConta, verifica se há saldo ou dívidas, caso não tenha nenhum dos dois
+o seu status é alterado para desativado.
+*/
     function fecharConta(){
         if ($this->getSaldo() < 0){
             echo "<p>A conta está em débito!</p>";
@@ -70,6 +80,9 @@ class ContaBanco {
           echo "<p>Conta fechada com sucesso!</p>";
         }
     }
+/* Função depositar, verifica se o status da conta está ativo, caso esteja, é creditado na conta uma quantidade
+de saldo na conta.
+*/
     function depositar($deposito){
        if ($this->getStatus() == true){
         $this->setSaldo($this->getSaldo() + $deposito);
@@ -77,6 +90,9 @@ class ContaBanco {
            echo "<p>Conta fechada, não consigo depositar!</p>";
        }
     }
+/* Função sacar, verifica se o status está ativo e se tem dinheiro na conta, caso esses requisitos estejam OK,
+é debitado na conta uma certa quantidade de saldo (desde que não ultrapasse o saldo atual da conta)
+*/
     function sacar($saque){   
         if ($this->getStatus() == true){
             if ($this -> getSaldo() >= 0){
@@ -92,6 +108,9 @@ class ContaBanco {
             }
         
     }
+/* Sempre que chamar essa função, é debitado 12 para conta corrente ou 20 para conta poupança
+Se o status da conta esteja desativado, é mostrado uma mensagem dizendo que a conta está fechada
+*/
     function pagarMensal(){       
         if ($this->getStatus() == true) {
             if ($this->getTipo()=="CC") {
@@ -109,6 +128,9 @@ class ContaBanco {
          echo "Conta fechada!";   
         }
     }
+/*
+Método contrutor
+*/
     public function ContaBanco(){
         $this->setSaldo(0);
         $this->setStatus(false);
